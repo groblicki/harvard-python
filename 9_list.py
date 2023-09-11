a6688a07cf9921c83586968257521d0aaafccd21
groblicki <damian.groblicki@gmail.com>
2023-09-11 17:50:28 +0200

    Learning list

---
+++ 9_list.py
@@ -0,0 +1,11 @@
+def main():
+    cats = ["Poppy", "Bella", "Misty", "Molly", "Daisy", "Tilly", "Luna", "Lily"]
+
+    for cat in cats:
+        print(f"{cat} says meow")
+
+    # for i in range(len(cats)):
+    #     print(f"{cats[i]} says meow")
+
+
+main()
