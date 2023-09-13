40496bd3563afb41f5a132b76b46304673dc35fa
groblicki <damian.groblicki@gmail.com>
2023-09-13 17:28:26 +0200

    Learning my own library

--- 14_packages.py
+++ 14_packages.py
@@ -1,11 +1,14 @@
 import sys
 import requests
 import cowsay
+import sayings
 
 # import json
 
 if len(sys.argv) == 2:
+    sayings.hello(sys.argv[1])
     response = requests.get(
         "https://timeapi.io/api/Time/current/zone?timeZone=Europe/Warsaw"
     )
-    cowsay.cow(f"Hello, {sys.argv[1]}!\nIt is {response.json()['time']}")
+    cowsay.cow(f"It is {response.json()['time']}")
+    sayings.goodbye(sys.argv[1])
