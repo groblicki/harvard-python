fe84d8483d769c70cd25907938c96d437f624e02
groblicki <damian.groblicki@gmail.com>
2023-09-14 10:57:27 +0200

    Change name of library

--- 14_packages.py
+++ 14_packages.py
@@ -1,14 +1,14 @@
 import sys
 import requests
 import cowsay
-import sayings
+import lib_sayings
 
 # import json
 
 if len(sys.argv) == 2:
-    sayings.hello(sys.argv[1])
+    lib_sayings.hello(sys.argv[1])
     response = requests.get(
         "https://timeapi.io/api/Time/current/zone?timeZone=Europe/Warsaw"
     )
     cowsay.cow(f"It is {response.json()['time']}")
-    sayings.goodbye(sys.argv[1])
+    lib_sayings.goodbye(sys.argv[1])
